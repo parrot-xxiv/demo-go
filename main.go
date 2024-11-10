174f2858ba25050106d574a886ff53d111528cb9
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2024-11-10 23:39:03 +0800

    create option selector

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
-	"demo-go/greet"
 	"fmt"
 )
 
+func checkLength() bool {
+	var password string
+	fmt.Println("Enter password:")
+	fmt.Scanln(&password)
+	return len(password) >= 5
+}
+
+// Dummy functions to simulate options
+func optionA() {
+	fmt.Println("Option A selected")
+}
+
+func optionB() {
+	fmt.Println("Option B selected")
+}
+
 func main() {
-	var name string
+	for {
+		var x int
+		fmt.Println("Choose app: ")
+		fmt.Println("1 Option A")
+		fmt.Println("2 Option B")
+		fmt.Println("3 Exit")
+		fmt.Scan(&x)
 
-	fmt.Println("Enter your name:")
-	fmt.Scanln(&name)
-
-	greet.Greet(name)
+		switch x {
+		case 1:
+			optionA()
+		case 2:
+			optionB()
+		case 3:
+			fmt.Println("Exited")
+			return // return instead of break
+		default:
+			fmt.Println("Not an option")
+		}
+	}
 }
